36fa3509a907bf5ee5d33745664fd8b5911ce9d7
Suthan-T <suthanthadeus2006@gmail.com>
2026-02-20 18:25:34 +0530

    database engine has changed

--- backend/database.py
+++ backend/database.py
@@ -8,7 +8,10 @@ SQLALCHEMY_DATABASE_URL = os.getenv(
     "postgresql://postgres:suthan06@localhost:5432/bragboard"
 )
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    pool_pre_ping=True
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
