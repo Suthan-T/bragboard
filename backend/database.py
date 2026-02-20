5360d5e2e8bbe0bcc01e33d6722f0422cea2579c
Suthan-T <suthanthadeus2006@gmail.com>
2026-02-20 17:57:03 +0530

    Updated database config to use environment variables

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:suthan06@localhost:5432/bragboard"
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://postgres:suthan06@localhost:5432/bragboard"
+)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
